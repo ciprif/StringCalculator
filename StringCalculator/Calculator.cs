0c670e5506efda4af147bcb689e9de1d2e573e47
Ciprian Filipas <cipri.filipas@gmail.com>
2015-04-01 10:37:06 +0300

    Initial Commit

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public string Add(params string[] numbers)
+        {
+            throw new NotImplementedException("The method is not implemented");
+        }
+    }
+}
---
+++ StringCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(c.Add("3", "5", "ana", "245"));
+            Console.ReadLine();
+        }
+    }
+}
